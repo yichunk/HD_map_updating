f67688ca7ce03a116db46a6260892e4209bbb5ee
yi-tinglin <yi-tinglin@tejaskhot.sp.cs.cmu.edu>
2020-10-27 21:39:34 -0400

    can add annotation

--- sign_detector/sythetic_dataset/make_sythetic_dataset.py
+++ sign_detector/sythetic_dataset/make_sythetic_dataset.py
@@ -2,9 +2,35 @@ from PIL import Image, ImageDraw, ImageFilter
 import json
 import random
 
-def random_paste_one_object(foreground, background, backgroubd_ann):
+id_to_sign_labe = [ '',      #0
+                    'regulatory--stop--g1', #1 
+                    'regulatory--yield--g1', #2
+                    'regulatory--no-entry--g1', #3 
+                    'regulatory--no-parking--g2', #4
+                    'regulatory--maximum-speed-limit-15--g3', #5
+                    'regulatory--turn-right--g3', #6
+                    'regulatory--go-straight--g3', #7
+                    'regulatory--turn-left--g2', #8
+                    'regulatory--no-right-turn--g1', #9
+                    'regulatory--no-straight-through--g1', #10
+                    'regulatory--no-left-turn--g1', #11
+                    'warning--pedestrians-crossing--g4' #12
+                   ]
+
+def save_obj(obj, file_path):
+    if not os.path.exists(file_path):
+        os.makedirs(file_path)
+
+    with open(file_path, 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+def load_obj(file_path):
+    with open(file_path, 'rb') as f:
+        return pickle.load(f)
+
+def random_paste_one_object(foreground, background, backgroubd_ann, object_label):
     img_w, img_h = background.size
-    obj_to_background_ratio = random.uniform(0.01, 0.19)
+    obj_to_background_ratio = random.uniform(0.01, 0.05)
     offset_x = round(random.uniform(0.2, 0.8) * img_w)
     offset_y = round(random.uniform(0.2, 0.8) * img_h)
     
@@ -15,18 +41,28 @@ def random_paste_one_object(foreground, background, backgroubd_ann):
     foreground = foreground.resize((new_obj_w, new_obj_h))
     background.paste(foreground, (offset_x, offset_y))
 
+    new_obj = backgroubd_ann['objects'][-1].copy()
+    new_obj['bbox']['xmin'] = offset_x
+    new_obj['bbox']['ymin'] = offset_y
+    new_obj['bbox']['xmax'] = offset_x + new_obj_w
+    new_obj['bbox']['ymax'] = offset_y + new_obj_h
+    new_obj['label'] = object_label
+    backgroubd_ann['objects'].append(new_obj)
+
     
 if __name__ == "__main__":
     im1 = Image.open('MTSD_samples/North_America/f82itz69069z_LC7xM6UIA.jpg')
-    im2 = Image.open('MTSD_samples/7_5_6.jpg')
+    im2 = Image.open('MTSD_samples/7_5_6.jpg') #image_id_class_id_num_object
 
 
     ann1 = None
     with open('MTSD_samples/North_America/f82itz69069z_LC7xM6UIA.json') as json_file:
         ann1 = json.load(json_file) #annotation_data is a python dict
 
-    print(ann1['objects'])
-    #num_objects_max = len(annotation_data['objects'])
+    random_paste_one_object(im2, im1, ann1, id_to_sign_labe[5])
 
-    random_paste_one_object(im2, im1, ann1)
+    
+    with open('output.json', 'w') as outfile:
+        json.dump(ann1, outfile)
+    
     im1.save('output.jpg', quality=95)
